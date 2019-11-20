7dba559916e46396dc7e8c245e1af22ff722519a
Christer Nordbø <cnordb@equinor.com>
2019-11-20 12:02:11 +0100

    Remove duplicate check for auth redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,16 @@
-
 import React, { Suspense, useEffect } from 'react'
 
-import Header from '../modules/Header';
+import GeneralRouter from './GeneralRouter';
 import Spinner from '../components/Spinner';
 import { ThemeProvider } from 'styled-components';
 import { hot } from 'react-hot-loader';
 import theme from './../assets/theme';
 import { useAuth } from '../contexts/AuthContext';
-import GeneralRouter from './GeneralRouter';
 
 const Login = React.lazy(() => import('../modules/Login'));
 
 const App = () => {
     const auth = useAuth();
-    auth.handleRedirectCallback((err) => console.log("Redirect Err", err));
 
     useEffect(() => {
         console.log("AuthContext: ", auth);

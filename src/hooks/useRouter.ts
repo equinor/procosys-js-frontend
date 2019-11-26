8dc879e9ba7bd598579893158282d059d4dc33b0
Christer Nordbø <cnordb@equinor.com>
2019-11-26 13:48:27 +0100

    Add react router hook

---
+++ src/hooks/useRouter.ts
@@ -0,0 +1,23 @@
+import {useHistory, useLocation, useParams, useRouteMatch} from 'react-router-dom';
+
+import {useMemo} from 'react';
+
+const useRouter = (): any => {
+    const params = useParams();
+    const location = useLocation();
+    const history = useHistory();
+    const match = useRouteMatch();
+
+    return useMemo(() => {
+        return {
+            push: history.push,
+            replace: history.replace,
+            pathname: location.pathname,
+            match,
+            location,
+            history
+        };
+    }, [params, match, location, history]);
+};
+
+export default useRouter;

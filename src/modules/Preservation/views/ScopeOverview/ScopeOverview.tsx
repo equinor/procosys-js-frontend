1732c7a2307271239f7c7d2e37f4ece07291ca20
Kjetil Falnes <KFALN@equinor.com>
2020-01-13 10:10:55 +0100

    Removed custom Button component (replaced by EDS).

--- src/modules/Preservation/views/ScopeOverview/ScopeOverview.tsx
+++ src/modules/Preservation/views/ScopeOverview/ScopeOverview.tsx
@@ -5,8 +5,6 @@ import { Select } from '../../../../components';
 import { SelectItem } from '../../../../components/Select';
 import { usePreservationContext } from '../../context/PreservationContext';
 
-import { Button } from '@equinor/eds-core-react';
-
 const ScopeOverview: React.FC = (): JSX.Element => {
 
     const { project, availableProjects, setCurrentProject } = usePreservationContext();
@@ -59,26 +57,6 @@ const ScopeOverview: React.FC = (): JSX.Element => {
                         <td><input type="checkbox" /></td>
                         <td>T-1000-SJD</td>
                         <td>This is a tag</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Button onClick={(): void => alert('ohoi')}>Primary EDS</Button></td>
-                        <td><Button color="secondary">Secondary EDS</Button></td>
-                        <td><Button disabled>Disabled EDS</Button></td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Button color="danger">Danger EDS</Button></td>
-                        <td><Button variant="outlined">Outlined EDS</Button></td>
-                        <td><Button variant="ghost">Ghost EDS</Button></td>
                     </tr>
                 </tbody>
             </table>

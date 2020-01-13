027397e19b21b79d383b4535c3b697e9dfa52681
Kjetil Falnes <KFALN@equinor.com>
2020-01-13 09:13:17 +0100

    Added @equinor/eds-core-react package

--- src/modules/Preservation/views/ScopeOverview/ScopeOverview.tsx
+++ src/modules/Preservation/views/ScopeOverview/ScopeOverview.tsx
@@ -1,13 +1,15 @@
 import { Container, Header } from './ScopeOverview.style';
-import React, {useMemo} from 'react';
+import React, { useMemo } from 'react';
 
 import { Select } from '../../../../components';
 import { SelectItem } from '../../../../components/Select';
 import { usePreservationContext } from '../../context/PreservationContext';
 
+import { Button } from '@equinor/eds-core-react';
+
 const ScopeOverview: React.FC = (): JSX.Element => {
 
-    const {project, availableProjects, setCurrentProject} = usePreservationContext();
+    const { project, availableProjects, setCurrentProject } = usePreservationContext();
 
     const projectSelectOptions = useMemo(() => {
         return availableProjects.map(project => {
@@ -26,7 +28,7 @@ const ScopeOverview: React.FC = (): JSX.Element => {
         <Container>
             <Header>
                 <h1>Preservation tags</h1>
-                <Select data={projectSelectOptions} selected={{text: project.description, value: project.id}} onChange={changeProject} />
+                <Select data={projectSelectOptions} selected={{ text: project.description, value: project.id }} onChange={changeProject} />
             </Header>
             <h3>{project.description}</h3>
             <table>
@@ -57,6 +59,26 @@ const ScopeOverview: React.FC = (): JSX.Element => {
                         <td><input type="checkbox" /></td>
                         <td>T-1000-SJD</td>
                         <td>This is a tag</td>
+                    </tr>
+                    <tr>
+                        <td>-</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><Button onClick={(): void => alert('ohoi')}>Primary EDS</Button></td>
+                        <td><Button color="secondary">Secondary EDS</Button></td>
+                        <td><Button disabled>Disabled EDS</Button></td>
+                    </tr>
+                    <tr>
+                        <td>-</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><Button color="danger">Danger EDS</Button></td>
+                        <td><Button variant="outlined">Outlined EDS</Button></td>
+                        <td><Button variant="ghost">Ghost EDS</Button></td>
                     </tr>
                 </tbody>
             </table>

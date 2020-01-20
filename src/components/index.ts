de82720db8067b17d1d754f839953d03fe7e28dc
Arild Bjerkenes <arild.bjerkenes@gmail.com>
2020-01-20 11:28:42 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Dropdown } from './Dropdown/index';
-export { default as Loading } from './Loading/index';
-export { default as Select } from './Select/index';
+export {default as Dropdown} from './Dropdown/index';
+export {default as Loading} from './Loading/index';
+export {default as Select} from './Select/index';

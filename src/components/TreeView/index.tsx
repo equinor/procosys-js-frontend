1fff52756a4a3cf5aa1f64d9c4bacf9ebb5f5398
Kjetil Falnes <kfaln@equinor.com>
2020-03-18 07:41:10 +0100

    TreeView component (#54)

---
+++ src/components/TreeView/index.tsx
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { TreeContainer, NodeContainer, ExpandCollapseIcon, NodeName, NodeLink } from './style';
+
+/**
+ * @param id Unique identifier across all nodes in the tree (number or string).
+ * @param name Node display name.
+ * @param getChildren Optional: function to load children.
+ * @param onClick Optional: function to handle click event on node.
+ */
+export interface TreeViewNode {
+    id: number | string;
+    name: string;
+    getChildren?: () => TreeViewNode[];
+    onClick?: () => void;
+}
+
+// internal props
+interface NodeData extends TreeViewNode {
+    parentId?: number | string;
+    isExpanded?: boolean;
+    children?: NodeData[];
+}
+
+interface TreeViewProps {
+    rootNodes: TreeViewNode[];
+}
+
+const TreeView = ({
+    rootNodes
+}: TreeViewProps): JSX.Element => {
+
+    const [treeData, setTreeData] = useState<NodeData[]>(rootNodes);
+
+    const collapseNode = (node: NodeData): void => {
+        // set collapsed state
+        node.isExpanded = false;
+
+        const collapsingNodeId = node.id;
+        const collapsingNodeIndex = treeData.findIndex(data => data.id === collapsingNodeId);
+
+        let childCount = 0;
+
+        // get number of child nodes to remove
+        treeData.forEach(treeNode => {
+            let nodeParentId: number | string | null | undefined = treeNode.parentId;
+
+            while (nodeParentId) {
+                // check whether the child exists under node being collapsed
+                if (nodeParentId === collapsingNodeId) {
+                    treeNode.isExpanded = false;
+                    childCount++;
+                }
+
+                // move up the tree
+                const parent = treeData.find(data => data.id === nodeParentId);
+                nodeParentId = parent ? parent.parentId : null;
+            }
+        });
+
+        // remove children after parent
+        const newTreeData = [...treeData];
+        newTreeData.splice(collapsingNodeIndex + 1, childCount);
+
+        setTreeData(newTreeData);
+    };
+
+    const expandNode = (node: NodeData): void => {
+        // set expanded state
+        node.isExpanded = true;
+
+        let children: NodeData[] = [];
+
+        if (node.children && node.children.length > 0) {
+            // use already loaded children
+            children = node.children;
+        } else {
+            // load children (TODO: need async handling)
+            children = node.getChildren ? node.getChildren() : [];
+
+            // set parent relation for all children
+            children.forEach(child => child.parentId = node.id);
+
+            // set child relations for the parent
+            node.children = children;
+        }
+
+        // insert children after parent
+        const parentIndex = treeData.findIndex(data => data.id === node.id);
+
+        const newTreeData = [...treeData];
+        newTreeData.splice(parentIndex + 1, 0, ...children);
+
+        setTreeData(newTreeData);
+    };
+
+    const getNodeIndentation = (node: NodeData): number => {
+        let indentMultiplier = 0;
+        let parentId: string | number | null | undefined = node.parentId;
+
+        // add indentation based on number of parents up the tree
+        while (parentId) {
+            const parent = treeData.find(node => node.id === parentId);
+
+            if (parent) {
+                parentId = parent.parentId;
+                indentMultiplier += 3;
+            } else {
+                parentId = null;
+            }
+        }
+
+        return indentMultiplier;
+    };
+
+    const getExpandCollapseIcon = (node: NodeData): JSX.Element | null => {
+        if (!node.getChildren) {
+            // node has no function for fetching children and cannot be expanded
+            return null;
+        }
+
+        const isExpanded = node.isExpanded === true;
+
+        return (
+            <ExpandCollapseIcon 
+                isExpanded={isExpanded}
+                onClick={(): void => {
+                    isExpanded ? collapseNode(node) : expandNode(node);
+                }}
+            >
+                { isExpanded && <KeyboardArrowDownIcon /> }
+                { !isExpanded && <KeyboardArrowRightIcon /> }
+            </ExpandCollapseIcon>  
+        );
+    };
+
+    const getNodeLink = (node: NodeData): JSX.Element => {
+        return (
+            <NodeName 
+                hasChildren={node.getChildren ? true : false} 
+                isExpanded={node.isExpanded === true}>
+                {
+                    node.onClick && (
+                        <NodeLink
+                            isExpanded={node.isExpanded === true} 
+                            onClick={node.onClick}
+                        >
+                            {node.name}
+                        </NodeLink>
+                    )
+                }
+                {
+                    !node.onClick && node.name
+                }
+            </NodeName>
+        );
+    };
+
+    const getNode = (node: NodeData): JSX.Element => {
+        const indent = getNodeIndentation(node);
+
+        return (
+            <NodeContainer key={node.id} indentMultiplier={indent}>
+                { getExpandCollapseIcon(node) }
+                { getNodeLink(node) }
+            </NodeContainer>
+        );
+    };
+
+    return (
+        <TreeContainer>
+            {
+                treeData.map(node => getNode(node))
+            }            
+        </TreeContainer>
+    );
+};
+
+export default TreeView;
---
+++ src/components/TreeView/style.ts
@@ -0,0 +1,73 @@
+import styled, { css } from 'styled-components';
+import { tokens } from '@equinor/eds-tokens';
+
+export const TreeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+interface NodeContainerProps {
+    indentMultiplier: number;
+}
+
+export const NodeContainer = styled.div<NodeContainerProps>`
+    display: flex;
+    align-items: center;
+    margin-bottom: var(--grid-unit);
+
+    margin-left: ${(props): string => `calc(var(--grid-unit) * ${props.indentMultiplier} - 4px)`};
+`;
+
+interface ExpandCollapseIconProps {
+    isExpanded: boolean;
+}
+
+export const ExpandCollapseIcon = styled.div<ExpandCollapseIconProps>`
+    cursor: pointer;
+    margin-right: var(--grid-unit);
+    padding: var(--grid-unit) calc(var(--grid-unit) + 2px);
+
+    :hover {
+        background: ${tokens.colors.interactive.primary__selected_highlight.rgba};
+        border-radius: 100%;
+    }
+
+    ${(props): any => props.isExpanded && css`
+        svg path {
+            fill: ${tokens.colors.interactive.primary__resting.rgba};
+        }
+    `}
+`;
+
+interface NodeNameProps {
+    hasChildren: boolean;
+    isExpanded: boolean;
+}
+
+export const NodeName = styled.div<NodeNameProps>`
+    padding: var(--grid-unit) 0;
+    font-weight: 500;
+
+    ${(props): any => props.isExpanded && css`
+        color: ${tokens.colors.interactive.primary__resting.rgba};
+    `}
+
+    /* add margin to nodes without children, to align with those that do (with expand/collapse icon) */
+    margin-left: ${(props): string => !props.hasChildren ? 'calc(var(--grid-unit) * 6.5)' : '0'};
+`;
+
+interface NodeLinkProps {
+    isExpanded: boolean;
+}
+
+export const NodeLink = styled.span<NodeLinkProps>`
+    cursor: pointer;
+
+    ${(props): any => props.isExpanded && css`
+        color: ${tokens.colors.interactive.primary__resting.rgba};
+    `}
+
+    :hover {
+        color: ${tokens.colors.interactive.primary__resting.rgba};
+    }
+`;

30185b992db06057b84de2bd193a053abe647d7d
Christer Nordbø <cnordb@equinor.com>
2020-04-14 13:21:51 +0200

    Fix Async tests

--- src/modules/Preservation/views/ScopeOverview/TagFlyout/PreservationTab/__tests__/PreservationTab.test.jsx
+++ src/modules/Preservation/views/ScopeOverview/TagFlyout/PreservationTab/__tests__/PreservationTab.test.jsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 
 import PreservationTab from '../PreservationTab';
+import { act } from 'react-dom/test-utils';
 
 const tagDetails = {
     id: 1,
@@ -21,24 +22,38 @@ jest.mock('../../../../../context/PreservationContext', () => ({
     usePreservationContext: jest.fn(() => {
         return {
             apiClient: {
-                getTagRequirements: jest.fn()
+                getTagRequirements: () => Promise.resolve(null)
             }
         };
     })
 }));
 
-describe('Module: <PreservationTab />', () => {
+describe('<PreservationTab />', () => {
 
-    it('Should render tag details', () => {
-        const { getByText } = render(<PreservationTab tagId={100} tagDetails={tagDetails} />);
+    it('Should render tag details', async () => {
+        await act(async () => {
+            const { getByText } = render(<PreservationTab tagId={100} tagDetails={tagDetails} />);
 
-        expect(getByText('tag-description')).toBeInTheDocument();
-        expect(getByText('journey-title')).toBeInTheDocument();
-        expect(getByText('journey-mode')).toBeInTheDocument();
-        expect(getByText('responsible-name')).toBeInTheDocument();
-        expect(getByText('commpkg-no')).toBeInTheDocument();
-        expect(getByText('mcpkg-no')).toBeInTheDocument();
-        expect(getByText('po-no')).toBeInTheDocument();
-        expect(getByText('area-code')).toBeInTheDocument();
+            expect(getByText('tag-description')).toBeInTheDocument();
+            expect(getByText('journey-title')).toBeInTheDocument();
+            expect(getByText('journey-mode')).toBeInTheDocument();
+            expect(getByText('responsible-name')).toBeInTheDocument();
+            expect(getByText('commpkg-no')).toBeInTheDocument();
+            expect(getByText('mcpkg-no')).toBeInTheDocument();
+            expect(getByText('po-no')).toBeInTheDocument();
+            expect(getByText('area-code')).toBeInTheDocument();
+        });
     });
+
+    it('Should render spinner when given no requirements', async () => {
+        await act(async () => {
+            const { getByTitle } = render(<PreservationTab tagId={100} tagDetails={tagDetails} />);
+
+            expect(getByTitle('Loading')).toBeInTheDocument();
+        });
+    });
+
+    it.todo('Should render as readonly');
+    it.todo('Should render as editable');
+    it.todo('Should render requirements');
 });

1d45e92a8ce3e966c1224e2e2490f50e74d03fd9
Christer Nordbø <cnordb@equinor.com>
2020-05-04 11:09:46 +0200

    Override default font when using material UI components

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -1,1 +1,10 @@
+import {createMuiTheme } from '@material-ui/core/styles';
+
+
+export const materialUIThemeOverrides = createMuiTheme({
+    typography: {
+        fontFamily: 'Equinor',
+    },
+});
+
 export default {};

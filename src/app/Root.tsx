d30704eab1c435bca2a0d6aba31ed19bf9af532c
Christer Nordbø <cnordb@equinor.com>
2020-05-26 09:15:38 +0200

    re-structure root application loading

---
+++ src/app/Root.tsx
@@ -0,0 +1,33 @@
+import '../assets/sass/procosys-styles.scss';
+import {hot} from 'react-hot-loader';
+
+import ProcosysContext, { createProcosysContext } from '../core/ProcosysContext';
+import React, { useRef } from 'react';
+
+import App from './index';
+import { IAuthService } from 'src/auth/AuthService';
+
+type AppProps = {
+    authService: IAuthService;
+}
+
+const Root = (props: AppProps): JSX.Element => {
+    const rootRef = useRef<HTMLDivElement | null>(null);
+    const overlayRef = useRef<HTMLDivElement | null>(null);
+    const context = createProcosysContext({
+        auth: props.authService,
+    });
+
+    return (
+        <ProcosysContext.Provider value={context}>
+            <div id="procosys-root" ref={rootRef}>
+                <App />
+            </div>
+            <div id="procosys-overlay" ref={overlayRef}>
+                {/* Empty on purpose */}
+            </div>
+        </ProcosysContext.Provider>
+    );
+};
+
+export default hot(module)(Root);

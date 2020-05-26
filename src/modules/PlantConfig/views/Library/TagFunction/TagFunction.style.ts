a03ab81c38e22418bfa1356b0c04e64de3b6106a
Christer Nordbø <cnordb@equinor.com>
2020-05-26 11:32:55 +0200

    Initial Library styling

---
+++ src/modules/PlantConfig/views/Library/TagFunction/TagFunction.style.ts
@@ -0,0 +1,75 @@
+import styled, {css} from 'styled-components';
+import { tokens } from '@equinor/eds-tokens';
+
+export const Breadcrumbs = styled.section`
+    display: flex;
+    border-bottom: 1px solid ${tokens.colors.ui.background__medium.rgba};
+    align-items: center;
+    width: 100%;
+    padding: var(--grid-unit);
+`;
+
+export const Divider = styled.div`
+    margin-top: calc(var(--margin-module--top) * -1);
+    margin-bottom: -1000px;
+    margin-right: calc(var(--grid-unit) * 2);
+    margin-left: calc(var(--grid-unit) * 5);
+    border-left: solid 1px ${tokens.colors.ui.background__medium.rgba};
+`;
+
+export const DetailsSection = styled.section`
+    background-color: ${tokens.colors.ui.background__light.rgba};
+    padding: calc(var(--grid-unit)*2) calc(var(--grid-unit)*4);
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    margin-right: calc(var(--margin-module--right) *-1);
+    margin-top: calc(var(--margin-module--top) *-1);
+`;
+
+export const InformationContainer = styled.div`
+    display: inline-flex;
+    margin: calc(var(--grid-unit)*2) 0px;
+    flex-direction: column;
+    background-color: ${tokens.colors.ui.background__default.rgba};
+    padding: calc(var(--grid-unit)*2) calc(var(--grid-unit)*4);
+    .inputRow {
+        display: flex;
+        flex-direction: row;
+        margin-top: calc(var(--grid-unit)*2)
+    }
+`;
+
+export const TabBar = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    padding: var(--grid-unit)  0px ;
+`;
+
+interface TabBarButtonProps {
+    current?: boolean;
+    disabled?: boolean;
+}
+export const TabBarButton = styled.div<TabBarButtonProps>`
+    border-bottom: 2px solid ${tokens.colors.ui.background__medium.rgba};
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    padding: var(--grid-unit) calc(var(--grid-unit)*2);
+    ${({current}): any => current && css`
+        color: ${tokens.colors.interactive.primary__resting.rgba};;
+        border-color: ${tokens.colors.interactive.primary__resting.rgba};
+    `}
+    ${({disabled}): any => disabled && css`
+        color: ${tokens.colors.interactive.disabled__text.rgba};
+    `}
+`;
+export const TabBarFiller = styled.div`
+    display: flex;
+    flex: 1;
+    border-bottom: 2px solid ${tokens.colors.ui.background__medium.rgba};
+
+`;
--- src/modules/PlantConfig/views/Library/TagFunction/TagFunction.tsx
+++ src/modules/PlantConfig/views/Library/TagFunction/TagFunction.tsx
@@ -1,27 +1,64 @@
 import React from 'react';
-
+import { Breadcrumbs } from './TagFunction.style';
+import {hot} from 'react-hot-loader';
+import {DetailsSection, Container, InformationContainer, TabBar, TabBarButton, TabBarFiller} from './TagFunction.style';
+import { TextField, Typography } from '@equinor/eds-core-react';
 
 type TagFunctionProps = {
     tagFunctionId: string;
 };
 
-
 const TagFunction = (props: TagFunctionProps): JSX.Element => {
-
-    //JUST IMPLEMENTED AS PROOF OF CONCEPT
-
     return (
 
-        <div>
-            <h3>TagFunction</h3>
+        <Container>
+            <Breadcrumbs>Library / Tag Function / ....</Breadcrumbs>
+            <DetailsSection>
+                <Typography variant="h3">TF.test, TF.Description</Typography>
+                <InformationContainer>
+                    <div className="inputRow">
+                        <TextField id="inp_register" label="Register" disabled />
+                    </div>
+                    <div className="inputRow">
+                        <TextField id="inp_tag_function" label="Tag Function" disabled />
+                    </div>
+                    <div className="inputRow">
+                        <TextField id="inp_description" label="Description" disabled />
+                    </div>
+                </InformationContainer>
+            </DetailsSection>
+            <section>
+                <TabBar>
+                    <TabBarButton current>
+                        Completion
+                    </TabBarButton>
+                    <TabBarButton disabled>
+                        Preservation
+                    </TabBarButton>
+                    <TabBarButton>
+                        DCCL
+                    </TabBarButton>
+                    <TabBarButton>
+                        CPCL
+                    </TabBarButton>
+                    <TabBarButton>
+                        Running logs
+                    </TabBarButton>
+                    <TabBarButton>
+                        Document requirement
+                    </TabBarButton>
+                    <TabBarButton>
+                        SPIR Requirement
+                    </TabBarButton>
+                    <TabBarFiller>
+                        {/* Just fills out the empty space */}
+                    </TabBarFiller>
+                </TabBar>
+                <section>{props.tagFunctionId}</section>
+            </section>
 
-            <div>
-                {props.tagFunctionId}
-            </div>
-
-        </div>
+        </Container>
     );
 };
 
-
-export default TagFunction;
+export default hot(module)(TagFunction);

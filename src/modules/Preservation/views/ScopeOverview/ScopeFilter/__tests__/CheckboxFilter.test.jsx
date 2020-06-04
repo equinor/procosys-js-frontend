05774a4d9cf282d1b7d13c71bc837c593e6c20d4
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2020-06-04 09:23:26 +0200

    added icon when rendering component in tests

--- src/modules/Preservation/views/ScopeOverview/ScopeFilter/__tests__/CheckboxFilter.test.jsx
+++ src/modules/Preservation/views/ScopeOverview/ScopeFilter/__tests__/CheckboxFilter.test.jsx
@@ -19,12 +19,13 @@ const filterValues =
         }
     ];
 
+const testEdsIcon = 'edit';
 
 describe('<CheckboxFilter />', () => {
 
     it('Should render checkbox section with title and checkboxes with initial value.', () => {
         const title = 'TestFilter';
-        const {getByTestId, getByLabelText} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' itemsChecked={['1']} />);
+        const {getByTestId, getByLabelText} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' itemsChecked={['1']} icon={testEdsIcon} />);
         getByTestId('CheckboxHeader').click();
         expect(getByLabelText(filterValues[0].title)).toBeInTheDocument();
         expect(getByLabelText(filterValues[1].title)).toBeInTheDocument();
@@ -37,7 +38,7 @@ describe('<CheckboxFilter />', () => {
     it('Should trigger onCheckboxFilterChange when changed', () => {
         const spyFunction = jest.fn();
         const title = 'TestFilter';
-        const {getByTestId, getByLabelText} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1']} />);
+        const {getByTestId, getByLabelText} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1']} icon={testEdsIcon} />);
         getByTestId('CheckboxHeader').click();
         getByLabelText(filterValues[1].title).click();
         expect(spyFunction).toHaveBeenCalledTimes(1);
@@ -48,8 +49,8 @@ describe('<CheckboxFilter />', () => {
     it('Value should change on prop change', () => {
         const spyFunction = jest.fn();
         const title = 'TestFilter';
-        const {getByTestId, getByLabelText, rerender} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1']} />);
-        rerender(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1', '2']} />);
+        const {getByTestId, getByLabelText, rerender} = render(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1']} icon={testEdsIcon} />);
+        rerender(<CheckboxFilter title={title} filterValues={filterValues} tagListFilterParam='testFilters' onCheckboxFilterChange={spyFunction} itemsChecked={['1', '2']} icon={testEdsIcon} />);
         getByTestId('CheckboxHeader').click();
         expect(getByLabelText(filterValues[0].title)).toHaveAttribute('checked');
         expect(getByLabelText(filterValues[1].title)).toHaveAttribute('checked');

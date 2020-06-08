bdee61b09dec18191c3bbfd597919c4aa26ac53f
Christer Nordbø <cnordb@equinor.com>
2020-06-08 10:42:33 +0200

    Use template as settings source for tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
+jest.mock('./settings.json', () => {
+    const settingsTemplate = require('./settings.template.json');
+    return settingsTemplate;
+});

05c8b06d151d56b7bf3ecaee44f6b1d7e46f3b80
Christer Nordbø <cnordb@equinor.com>
2020-07-01 16:12:02 +0200

    Add a default requirement icon, sort list

--- src/components/PreservationIcon.tsx
+++ src/components/PreservationIcon.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-
+import AreaIcon from '../assets/icons/Area';
 import BatteryChargingFullOutlinedIcon from '@material-ui/icons/BatteryChargingFullOutlined';
 import BearingIcon from '../assets/icons/Bearing';
 import BuildOutlinedIcon from '@material-ui/icons/BuildOutlined';
 import FlashOnOutlinedIcon from '@material-ui/icons/FlashOnOutlined';
 import N2Icon from '../assets/icons/N2';
+import OtherIcon from '@material-ui/icons/AddToPhotosOutlined';
 import PowerOutlinedIcon from '@material-ui/icons/PowerOutlined';
 import PressureIcon from '../assets/icons/Pressure';
+import React from 'react';
 import RotateRightIcon from '@material-ui/icons/RotateRightOutlined';
 import ThermostatIcon from '../assets/icons/Thermostat';
-import AreaIcon from '../assets/icons/Area';
 
 interface IconProps {
     variant: string;
@@ -19,28 +19,29 @@ const PreservationIcon = ({
     variant
 }: IconProps): JSX.Element | null => {
     switch (variant.toLowerCase()) {
-        case 'rotation':
-            return <RotateRightIcon />;
-        case 'ir test':
-            return <FlashOnOutlinedIcon />;
-        case 'oil level':
-            return <PressureIcon />;
-        case 'heating':
-            return <ThermostatIcon />;
-        case 'powered':
-            return <PowerOutlinedIcon />;
-        case 'vci':
-            return <BuildOutlinedIcon />;
-        case 'nitrogen':
-            return <N2Icon />;
-        case 'grease':
-            return <BearingIcon />;
-        case 'charging':
-            return <BatteryChargingFullOutlinedIcon />;
+
         case 'area':
             return <AreaIcon />;
+        case 'charging':
+            return <BatteryChargingFullOutlinedIcon />;
+        case 'grease':
+            return <BearingIcon />;
+        case 'heating':
+            return <ThermostatIcon />;
+        case 'ir test':
+            return <FlashOnOutlinedIcon />;
+        case 'nitrogen':
+            return <N2Icon />;
+        case 'oil level':
+            return <PressureIcon />;
+        case 'powered':
+            return <PowerOutlinedIcon />;
+        case 'rotation':
+            return <RotateRightIcon />;
+        case 'vci':
+            return <BuildOutlinedIcon />;
         default:
-            return null;
+            return <OtherIcon />;
     }
 };
 

22d22984f412b7091b079848a4b22e5e43700b3d
Pål Eie <PAEIE@equinor.com>
2020-09-16 15:02:07 +0200

    Context and api client for ipo

--- src/modules/PlantConfig/context/PlantConfigContext.tsx
+++ src/modules/PlantConfig/context/PlantConfigContext.tsx
@@ -4,10 +4,12 @@ import propTypes from 'prop-types';
 import { useCurrentPlant } from '../../../core/PlantContext';
 import { useProcosysContext } from '../../../core/ProcosysContext';
 import PreservationApiClient from '@procosys/modules/Preservation/http/PreservationApiClient';
+import InvitationForPunchOutApiClient from '@procosys/modules/InvitationForPunchOut/http/InvitationForPunchOutApiClient';
 
 type PlantConfigContextProps = {
     libraryApiClient: LibraryApiClient;
     preservationApiClient: PreservationApiClient;
+    invitationForPunchOutApiClient: InvitationForPunchOutApiClient
 }
 
 const PlantConfigContext = React.createContext<PlantConfigContextProps>({} as PlantConfigContextProps);
@@ -18,17 +20,21 @@ export const PlantConfigContextProvider: React.FC = ({ children }): JSX.Element 
     const { plant } = useCurrentPlant();
     const libraryApiClient = useMemo(() => new LibraryApiClient(auth), [auth]);
     const preservationApiClient = useMemo(() => new PreservationApiClient(auth), [auth]);
+    const invitationForPunchOutApiClient = useMemo(() => new InvitationForPunchOutApiClient(auth), [auth]);
+    
 
     useMemo(() => {
         libraryApiClient.setCurrentPlant(plant.id);
         preservationApiClient.setCurrentPlant(plant.id);
+        invitationForPunchOutApiClient.setCurrentPlant(plant.id);
     }, [plant]);
 
 
     return (
         <PlantConfigContext.Provider value={{
             libraryApiClient: libraryApiClient,
-            preservationApiClient: preservationApiClient
+            preservationApiClient: preservationApiClient,
+            invitationForPunchOutApiClient: invitationForPunchOutApiClient
         }}>
             {children}
         </PlantConfigContext.Provider>

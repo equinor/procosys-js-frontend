33ba9507d9954a44fdcce509b5233be0c4086f61
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2020-09-17 16:15:33 +0200

    removed unused styling and fix name of test

--- src/modules/InvitationForPunchOut/style.ts
+++ src/modules/InvitationForPunchOut/style.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     height: 100%;
-    h1 {
-        font-size: 36px;
-    }
     overflow-y: hidden;
 `;
 

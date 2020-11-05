6440714c606acb6b1db2ed4c73d5a6029471127f
Jan Erik Hagevold <JEHAG@equinor.com>
2020-11-05 08:57:46 +0100

    Fix tests

--- src/modules/InvitationForPunchOut/views/ViewIPO/Attachments/__tests__/Attachments.test.jsx
+++ src/modules/InvitationForPunchOut/views/ViewIPO/Attachments/__tests__/Attachments.test.jsx
@@ -1,4 +1,4 @@
-import { act, render, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import Attachments from '../index';
 import React from 'react';
@@ -16,15 +16,13 @@ jest.mock('../../../../context/InvitationForPunchOutContext',() => ({
 describe('Module: <Attachments ipoId={} />', () => {
 
     it('Should render empty table when no attachments', async () => {
-        let result;
-        await act(async () => Promise.resolve(result = render(<Attachments ipoId={2} />)));
+        const result = render(<Attachments ipoId={2} />);
 
-        expect(result.getByText('No records to display')).toBeInTheDocument();
+        await waitFor(() => expect(result.getByText('No records to display')).toBeInTheDocument());
     });
 
     it('Should render attachments in table', async () => {
-        let result;
-        await act(async () => Promise.resolve(result = render(<Attachments ipoId={0} />)));
+        const result = render(<Attachments ipoId={0} />);
 
         await waitFor(() => expect(result.getByText('file1.txt')).toBeInTheDocument());
     });

bc47d84ef6a511f38e6a9317c3f91e0c0ad43ff5
Jan Erik Hagevold <JEHAG@equinor.com>
2020-12-01 09:19:10 +0100

    Add tests for time value

--- src/modules/InvitationForPunchOut/views/CreateIPO/__tests__/CreateIPO.test.tsx
+++ src/modules/InvitationForPunchOut/views/CreateIPO/__tests__/CreateIPO.test.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, waitFor, within } from '@testing-library/react';
 import CreateIPO from '../CreateIPO';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
+import { configure } from '@testing-library/dom';
 
 jest.mock('react-router-dom', () => ({
     useParams: (): {projectId: any, commPkgNo: any} => ({
@@ -58,6 +59,32 @@ describe('<CreateIPO />', () => {
     it('Should render Previous button enabled', async () => {
         const { getByText } = render(<CreateIPO />);
         await waitFor(() => expect(getByText('Previous').closest('button')).toHaveProperty('disabled', true));
+    });
+
+    it('Should update end time when entering start time > end time', async () => {
+        configure({ testIdAttribute: 'id' }); // makes id attibute data-testid for subsequent tests
+        const { getByTestId } = render(<CreateIPO />);
+        const startTime = getByTestId('startTime');
+        const endTime = getByTestId('endDate');
+        fireEvent.change(startTime, { target: { value: '22:23'}});
+        await waitFor(() => expect(endTime).toHaveValue('23:23'));
+    });
+
+    it('Should not update end time when entering start time < end time', async () => {
+        const { getByTestId } = render(<CreateIPO />);
+        const startTime = getByTestId('startTime');
+        const endTime = getByTestId('endDate');
+        fireEvent.change(endTime, { target: { value: '23:23'}});
+        fireEvent.change(startTime, { target: { value: '08:23'}});
+        await waitFor(() => expect(endTime).toHaveValue('23:23'));
+    });
+
+    it('Should not update time when invalid (empty)', async () => {
+        const { getByTestId } = render(<CreateIPO />);
+        const startTime = getByTestId('startTime');
+        fireEvent.change(startTime, { target: { value: '13:30'}});
+        fireEvent.change(startTime, { target: { value: ''}});
+        await waitFor(() => expect(startTime).toHaveValue('13:30'));
     });
 });
 

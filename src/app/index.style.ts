afc8cac555bb55272a0784672e200736cde8c303
Arild Bjerkenes <ARBJE@equinor.com>
2020-12-03 22:28:50 +0100

    Test fix for mobile

--- src/app/index.style.ts
+++ src/app/index.style.ts
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export const ProCoSysRootLayout = styled.div`
     display: flex;
     flex-direction: column;
-//    max-height: calc(100%);
+    max-height: calc(100vh);
     overflow-y: auto;
-    height: 100vh;
     min-height: 100vh;
-    //min-height: -webkit-fill-available;
+    min-height: -webkit-fill-available;
 
     #root-content {
         overflow: auto;

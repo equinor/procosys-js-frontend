ef5c5bfc19495fccd230afe77c9e240b3833bac7
Arild Bjerkenes <ARBJE@equinor.com>
2020-12-03 23:28:22 +0100

    Test fix for mobile

--- src/app/index.style.ts
+++ src/app/index.style.ts
@@ -3,13 +3,19 @@ import styled from 'styled-components';
 export const ProCoSysRootLayout = styled.div`
     display: flex;
     flex-direction: column;
-    max-height: calc(100vh);
+    //max-height: calc(100vh);
     overflow-y: auto;
     min-height: 100vh;
     min-height: -webkit-fill-available;
+    height: fill-available;
 
     #root-content {
         overflow: auto;
         height: 100%;
+    }
+    
+    html {
+        height: -webkit-fill-available;
+        height: fill-available;
     }
 `;

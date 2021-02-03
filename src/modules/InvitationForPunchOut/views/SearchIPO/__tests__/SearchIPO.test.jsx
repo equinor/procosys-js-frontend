47854ec8141515e82c4f4e99c6addf4b30f557d2
Jan Erik Hagevold <JEHAG@equinor.com>
2021-02-03 12:19:40 +0100

    Add test fix

--- src/modules/InvitationForPunchOut/views/SearchIPO/__tests__/SearchIPO.test.jsx
+++ src/modules/InvitationForPunchOut/views/SearchIPO/__tests__/SearchIPO.test.jsx
@@ -3,9 +3,9 @@ import SearchIPO from '../../SearchIPO';
 import { render } from '@testing-library/react';
 
 const mockIPOs = { maxAvailable: 3, ipos: [
-    {id: 0, title: 'IPO-11', status: 'Planned', type: 'MDP', mcPkgs: [{mcPkgNo: '1001-D03'}, {mcPkgNo: '25221-D01'}, {mcPkgNo: '32133-A03'}, {mcPkgNo: '32133-A03'}], sent: (new Date()).toUTCString(), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '},
-    {id: 1, title: 'IPO-13', status: 'Completed', type: 'DP', mcPkgs: [{mcPkgNo: '2001-D03'}], sent: (new Date(2012, 11,11,11,11)).toUTCString(), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '},
-    {id: 2, title: 'IPO-13', status: 'Canceled', type: 'DP', mcPkgs: [{mcPkgNo: '2001-D03'}], sent: (new Date(2012, 11,11,11,11)).toUTCString(), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '}
+    {id: 0, title: 'IPO-11', status: 'Planned', type: 'MDP', mcPkgs: [{mcPkgNo: '1001-D03'}, {mcPkgNo: '25221-D01'}, {mcPkgNo: '32133-A03'}, {mcPkgNo: '32133-A03'}], sent: new Date(), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '},
+    {id: 1, title: 'IPO-13', status: 'Completed', type: 'DP', mcPkgs: [{mcPkgNo: '2001-D03'}], sent: new Date(2012, 11,11,11,11), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '},
+    {id: 2, title: 'IPO-13', status: 'Canceled', type: 'DP', mcPkgs: [{mcPkgNo: '2001-D03'}], sent: new Date(2012, 11,11,11,11), contractorRep: 'asdasd asd ', constructionRep: 'dawdada dwa adw '}
 ] };
 
 

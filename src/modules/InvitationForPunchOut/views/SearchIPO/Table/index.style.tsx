ec6eda903159c6721ea7e39ee1f31f04178fa3ba
Jan Erik Hagevold <JEHAG@equinor.com>
2021-02-04 07:14:04 +0100

    Cleanup table css

--- src/modules/InvitationForPunchOut/views/SearchIPO/Table/index.style.tsx
+++ src/modules/InvitationForPunchOut/views/SearchIPO/Table/index.style.tsx
@@ -1,13 +1,7 @@
-import { Breakpoints } from '@procosys/core/styling';
 import styled from 'styled-components';
 import { tokens } from '@equinor/eds-tokens';
 
 export const Container = styled.div`
-    input + svg {
-        width: 24px;
-        height: 24px;       
-    }
-    
     tbody, thead {
         .MuiButtonBase-root {
             :hover {
@@ -33,30 +27,4 @@ export const Container = styled.div`
         text-overflow: ellipsis;
         color: inherit;
     }
-
-    
-    //Hide requirement column in the middle
-    thead tr th:nth-child(4), table tr td:nth-child(4)
-    { 
-        display:none;
-    }   
-    
-    ${Breakpoints.TABLET} {
-        //hide columns
-        thead tr th:nth-child(3), table tr td:nth-child(3) //description
-        {
-            display: none;            
-        } 
-        //Show requirement column in the middle
-        thead tr th:nth-child(4), table tr td:nth-child(4)
-        {   
-            display:table-cell;
-        }        
-        //Hide requirement column at the end
-        thead tr th:nth-child(13), table tr td:nth-child(13)
-        {   
-            display:none;
-        }               
-    }
-
 `;

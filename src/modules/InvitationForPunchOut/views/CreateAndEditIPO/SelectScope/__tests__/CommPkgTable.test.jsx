968ae55a4a3530e616fccda4f9f42014117194c7
Jan Erik Hagevold <JEHAG@equinor.com>
2021-02-10 09:56:29 +0100

    Fix test

--- src/modules/InvitationForPunchOut/views/CreateAndEditIPO/SelectScope/__tests__/CommPkgTable.test.jsx
+++ src/modules/InvitationForPunchOut/views/CreateAndEditIPO/SelectScope/__tests__/CommPkgTable.test.jsx
@@ -1,7 +1,6 @@
-import { render } from '@testing-library/react';
-
 import CommPkgTable from '../CommPkgTable';
 import React from 'react';
+import { render } from '@testing-library/react';
 
 describe('<CommPkgTable />', () => {
     it('Should render table', () => {

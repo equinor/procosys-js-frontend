988ee50ab6ebcd5eef895741093ee59785626414
Vilde Riveland <vilderiveland@gmail.com>
2021-03-04 12:56:50 +0100

    Feature/80981 UI new IPO button from search (#472)
    * Added New IPO button to IPO search
    
    * Fixed routing
    
    * Test updated

--- src/modules/InvitationForPunchOut/views/SearchIPO/__tests__/SearchIPO.test.jsx
+++ src/modules/InvitationForPunchOut/views/SearchIPO/__tests__/SearchIPO.test.jsx
@@ -2,6 +2,8 @@ import { render, waitFor } from '@testing-library/react';
 
 import React from 'react';
 import SearchIPO from '../../SearchIPO';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
 
 jest.mock('../../../context/InvitationForPunchOutContext', () => ({
     useInvitationForPunchOutContext: () => {
@@ -17,11 +19,15 @@ jest.mock('../../../context/InvitationForPunchOutContext', () => ({
 
 describe('<SearchIPO />', () => {
     it('Should render with header and empty table', async () => {
+        const history = createMemoryHistory();
         const { getByText } = render(
-            <SearchIPO />
+            <Router history={history}>
+                <SearchIPO />
+            </Router>
         );          
         await waitFor(() => expect(getByText('Invitation for punch-out')).toBeInTheDocument());
         await waitFor(() => expect(getByText('Select project')).toBeInTheDocument());
         await waitFor(() => expect(getByText('No records to display')).toBeInTheDocument());
+        await waitFor(() => expect(getByText('New IPO')).toBeInTheDocument());
     });
 });

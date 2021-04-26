9f66795dc63646838c36793dca842940de71f808
Lykke Larsen <NORL@statoil.com>
2021-04-26 09:22:42 +0200

    add general styling to highlight dates and times in datetime picker

--- src/app/index.style.ts
+++ src/app/index.style.ts
@@ -2,6 +2,18 @@ import { Breakpoints } from '@procosys/core/styling';
 import styled from 'styled-components';
 
 export const ProCoSysRootLayout = styled.div`
+input::-webkit-datetime-edit-ampm-field:focus,
+input::-webkit-datetime-edit-day-field:focus,
+input::-webkit-datetime-edit-hour-field:focus,
+input::-webkit-datetime-edit-millisecond-field:focus,
+input::-webkit-datetime-edit-minute-field:focus,
+input::-webkit-datetime-edit-month-field:focus,
+input::-webkit-datetime-edit-second-field:focus,
+input::-webkit-datetime-edit-week-field:focus,
+input::-webkit-datetime-edit-year-field:focus {
+ background-color: highlight;
+ color: highlighttext;
+}
     display: flex;
     flex-direction: column;
     //max-height: calc(100vh);

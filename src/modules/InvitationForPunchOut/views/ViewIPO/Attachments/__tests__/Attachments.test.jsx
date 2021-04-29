5a2b16b8623788cdbab4344154695d374a85cc83
VRiveland <vriv@equinor.com>
2021-04-29 10:32:51 +0200

    Fixed failing test

--- src/modules/InvitationForPunchOut/views/ViewIPO/Attachments/__tests__/Attachments.test.jsx
+++ src/modules/InvitationForPunchOut/views/ViewIPO/Attachments/__tests__/Attachments.test.jsx
@@ -47,7 +47,7 @@ describe('Module: <Attachments ipoId={} />', () => {
     it('Should render attachments in table', async () => {
         const { getByText } = render(<Attachments ipoId={0} />);
 
-        await waitFor(() => expect(getByText('file1')).toBeInTheDocument());
+        await waitFor(() => expect(getByText('file1.txt')).toBeInTheDocument());
         await waitFor(() => expect(getByText(getFormattedDateAndTime(attachment.uploadedAt))).toBeInTheDocument());
         await waitFor(() => expect(getByText(`${attachment.uploadedBy.firstName} ${attachment.uploadedBy.lastName}`)).toBeInTheDocument());
     });

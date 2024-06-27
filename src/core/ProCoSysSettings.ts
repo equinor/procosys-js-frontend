f6e5ad8df3d79feb5697b835e4caad3629dccbc1
kslazykv <kslaz@equinor.com>
2024-06-27 13:37:59 +0200

    changed path

--- src/core/ProCoSysSettings.ts
+++ src/core/ProCoSysSettings.ts
@@ -1,7 +1,7 @@
 import { IAuthService } from 'src/auth/AuthService';
 import SettingsApiClient from '@procosys/http/SettingsApiClient';
 
-const localSettings = require('../../settings.json');
+const localSettings = require('../settings.json');
 //#region types
 
 interface FeatureConfig {

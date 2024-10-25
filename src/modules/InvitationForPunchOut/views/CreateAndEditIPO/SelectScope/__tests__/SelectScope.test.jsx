a3f9113ca2ea5e470a6d3ca00949e8d110f2cba9
kslazykv <kslaz@equinor.com>
2024-10-25 07:51:38 +0200

    fixed tests

--- src/modules/InvitationForPunchOut/views/CreateAndEditIPO/SelectScope/__tests__/SelectScope.test.jsx
+++ src/modules/InvitationForPunchOut/views/CreateAndEditIPO/SelectScope/__tests__/SelectScope.test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import SelectScope from '../SelectScope';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('<SelectScope />', () => {
     it('Renders with correct title and table for MDP', async () => {
         var propFunc = jest.fn();
         const { getByText } = render(
-            <SelectScope
-                selectedMcPkgScope={propFunc}
-                setSelectedCommPkgScope={propFunc}
-            />
+            <MemoryRouter>
+                <SelectScope
+                    selectedMcPkgScope={propFunc}
+                    setSelectedCommPkgScope={propFunc}
+                />
+            </MemoryRouter>
         );
         expect(getByText('Select commissioning packages')).toBeInTheDocument();
         expect(getByText('Comm pkg')).toBeInTheDocument;
@@ -18,11 +21,13 @@ describe('<SelectScope />', () => {
     it('Renders with correct title and table for DP', async () => {
         var propFunc = jest.fn();
         const { getByText } = render(
-            <SelectScope
-                type="DP"
-                selectedMcPkgScope={propFunc}
-                setSelectedCommPkgScope={propFunc}
-            />
+            <MemoryRouter>
+                <SelectScope
+                    type="DP"
+                    selectedMcPkgScope={propFunc}
+                    setSelectedCommPkgScope={propFunc}
+                />
+            </MemoryRouter>
         );
         expect(
             getByText('Click on the arrow next to a comm pkg to open MC scope')
@@ -33,11 +38,13 @@ describe('<SelectScope />', () => {
     it('Renders with correct title for MDP if commPkgNo is given', async () => {
         var propFunc = jest.fn();
         const { getByText } = render(
-            <SelectScope
-                selectedMcPkgScope={propFunc}
-                commPkgNo={50}
-                setSelectedCommPkgScope={propFunc}
-            />
+            <MemoryRouter>
+                <SelectScope
+                    selectedMcPkgScope={propFunc}
+                    commPkgNo={50}
+                    setSelectedCommPkgScope={propFunc}
+                />
+            </MemoryRouter>
         );
         expect(getByText('Scope has been preselected')).toBeInTheDocument();
     });
@@ -45,12 +52,14 @@ describe('<SelectScope />', () => {
     it('Renders with correct title for DP if commPkgNo is given', async () => {
         var propFunc = jest.fn();
         const { getByText } = render(
-            <SelectScope
-                type="DP"
-                selectedMcPkgScope={propFunc}
-                commPkgNo={50}
-                setSelectedCommPkgScope={propFunc}
-            />
+            <MemoryRouter>
+                <SelectScope
+                    type="DP"
+                    selectedMcPkgScope={propFunc}
+                    commPkgNo={50}
+                    setSelectedCommPkgScope={propFunc}
+                />
+            </MemoryRouter>
         );
         expect(
             getByText('Select MC packages in comm pkg 50')

664f1b1ec7c9774d794cae67611bba81c3b76fe5
kslazykv <kslaz@equinor.com>
2024-10-28 12:45:32 +0100

    babel.config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
     presets: [
         '@babel/preset-env',
         ['@babel/preset-react', { runtime: 'automatic' }],
-        '@babel/preset-typescript',
     ],
     plugins: [
         //Lets us use React.lazy(() => {})

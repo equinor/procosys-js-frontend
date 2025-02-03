06816cbd3bfe65383ba39489758736331e792d82
Gustav Eikaas <guei@equinor.com>
2025-02-03 11:02:53 +0100

    redirectUri

--- src/auth/AuthService.tsx
+++ src/auth/AuthService.tsx
@@ -53,6 +53,7 @@ export default class AuthService implements IAuthService {
             auth: {
                 clientId: ProCoSysSettings.clientId,
                 authority: ProCoSysSettings.authority,
+                redirectUri: window.location.href,
             },
             cache: {
                 cacheLocation: 'sessionStorage', // This configures where your cache will be stored

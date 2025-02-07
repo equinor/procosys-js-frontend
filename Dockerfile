cc853de2e899818420848958c1261b56e33a0018
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2025-02-07 09:49:52 +0100

    feat: env mode handling (#907)

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:20 as build
+WORKDIR /app
+COPY . .
+RUN npm i -g pnpm@9.0.5
+RUN pnpm install
+
+RUN pnpm bundle
+
+FROM docker.io/nginxinc/nginx-unprivileged:alpine
+
+WORKDIR /app
+
+COPY --from=build /app/dist /usr/share/nginx/html
+COPY .docker/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
+COPY .docker/scripts/ /etc/scripts/
+
+USER 0
+RUN chown -R nginx /etc/nginx/conf.d \
+    && chown -R nginx /app \
+    && chown -R nginx /usr/share/nginx/html \
+    && chmod +x /etc/scripts/run-nginx.sh 
+
+ 
+USER 101
+EXPOSE 5000
+
+CMD ["sh","/etc/scripts/run-nginx.sh"]
--- src/modules/Header/ModuleTabs.tsx
+++ src/modules/Header/ModuleTabs.tsx
@@ -1,11 +1,9 @@
 import { SubNav } from './style';
 import { NavLink, useParams } from 'react-router-dom';
-import React from 'react';
 
 type ModuleTabsProps = {
     onClick?: () => void;
 };
-
 const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
     const params = useParams<any>();
 
@@ -29,8 +27,8 @@ const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
             </a>
             <a href="Documents">Document</a>
             <a href="Notification">Notification</a>
-            <a href="Hookup">Hookup</a>
-            {__DEV__ && (
+            {(import.meta as ImportMeta & { mode?: 'development' })?.mode ===
+            'development' ? (
                 <span onClick={props.onClick}>
                     <NavLink
                         className={({ isActive }: { isActive: boolean }) =>
@@ -41,10 +39,11 @@ const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
                         Plant Configuration
                     </NavLink>
                 </span>
-            )}
-            {!__DEV__ && (
+            ) : (
                 <span onClick={props.onClick}>
-                    <a href="PlantConfig">Plant Configuration</a>
+                    <a href={`/${params.plant}/PlantConfig`}>
+                        Plant Configuration
+                    </a>
                 </span>
             )}
         </SubNav>

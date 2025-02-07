67dd0644e6866623a95378e5765d659048cf395a
Gustav Eikaas <guei@equinor.com>
2025-02-07 08:30:26 +0100

    feat: env mode handling

--- src/modules/Header/ModuleTabs.tsx
+++ src/modules/Header/ModuleTabs.tsx
@@ -1,11 +1,9 @@
 import { SubNav } from './style';
 import { NavLink, useParams } from 'react-router-dom';
-import React from 'react';
 
 type ModuleTabsProps = {
     onClick?: () => void;
 };
-
 const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
     const params = useParams<any>();
 
@@ -30,7 +28,7 @@ const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
             <a href="Documents">Document</a>
             <a href="Notification">Notification</a>
             <a href="Hookup">Hookup</a>
-            {__DEV__ && (
+            {Object.hasOwn(import.meta, "mode") && (import.meta as ImportMeta & {mode: "development"}).mode === 'development' ? (
                 <span onClick={props.onClick}>
                     <NavLink
                         className={({ isActive }: { isActive: boolean }) =>
@@ -41,8 +39,7 @@ const ModuleTabs = (props: ModuleTabsProps): JSX.Element => {
                         Plant Configuration
                     </NavLink>
                 </span>
-            )}
-            {!__DEV__ && (
+            ) : (
                 <span onClick={props.onClick}>
                     <a href="PlantConfig">Plant Configuration</a>
                 </span>

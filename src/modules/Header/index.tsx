88bfdc7c9739702124ffb8a2f5696e7936ba080e
kslazykv <kslaz@equinor.com>
2025-03-17 08:20:02 +0100

    fixed changePlant

--- src/modules/Header/index.tsx
+++ src/modules/Header/index.tsx
@@ -151,10 +151,17 @@ const Header: React.FC = (): JSX.Element => {
         setSearchValue('');
         setSearchResult(undefined);
 
+        const newPlantId = filteredPlants[plantIndex].value;
         const currentPath = location.pathname.split('/');
-        currentPath[1] = filteredPlants[plantIndex].value;
-        const newPath = currentPath.join('/');
-        navigate(newPath);
+
+        if (currentPath.includes('libraryv2')) {
+            console.log('Detected libraryv2, resetting path...');
+            navigate(`/${newPlantId}/libraryv2/`);
+        } else {
+            currentPath[1] = newPlantId;
+            const newPath = currentPath.join('/');
+            navigate(newPath);
+        }
     };
 
     const cancelerRef = useRef<Canceler | null>();
